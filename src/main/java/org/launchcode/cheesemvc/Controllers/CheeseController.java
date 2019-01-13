bc8bc66ee90742e16457106bdd4dbdf5c51094a1
Lynelle Camp <lcamp118@live.com>
2019-01-12 21:41:30 -0600

    cheese mvc views lesson

--- src/main/java/org/launchcode/cheesemvc/Controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/Controllers/CheeseController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     @RequestMapping(value = "")
     public String index(Model model) {
@@ -31,8 +32,8 @@ public class CheeseController {
         return "cheese/add";
     }
     @RequestMapping( value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-    cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDesc) {
+    cheeses.put(cheeseName, cheeseDesc);
 
     //redirect to /cheese.
     return "redirect:";
