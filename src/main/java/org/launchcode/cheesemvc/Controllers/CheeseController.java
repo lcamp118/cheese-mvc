7e2239b939fa241cf2e2fc531fb4c36c67507d68
Lynelle Camp <lcamp118@live.com>
2019-01-13 18:04:09 -0600

    finished spring boot models lesson part 2

--- src/main/java/org/launchcode/cheesemvc/Controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/Controllers/CheeseController.java
@@ -1,8 +1,10 @@
 package org.launchcode.cheesemvc.Controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +15,10 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
-
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
@@ -31,25 +31,24 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDesc) {
-        Cheese newCheese = new Cheese(cheeseName, cheeseDesc);
-        cheeses.add(newCheese);
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+        CheeseData.add(newCheese);
 
         //redirect to /cheese.
         return "redirect:";
     }
         @RequestMapping(value = "remove", method = RequestMethod.GET)
         public String displayRemoveCheeseForm(Model model) {
-            model.addAttribute("cheeses", cheeses);
+            model.addAttribute("cheeses", CheeseData.getAll());
             model.addAttribute("title", "Remove Cheese");
             return "cheese/remove";
         }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheese) {
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
 
-        for (String aCheese : cheese) {
-            cheeses.remove(aCheese);
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
 
         return "redirect:";
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -4,10 +4,18 @@ public class Cheese {
 
         private String name;
         private String description;
+        private int cheeseId;
+        private static int nextId = 1; //(static field is shared with all instances of class)
 
         public Cheese(String name, String description) {
+            this(); //this will call constructor below to initialize id
             this.name = name;
             this.description = description;
+        }
+
+        public Cheese(){
+            cheeseId = nextId; //set cheeseId to value of static nextId
+            nextId++; //increment id and set nextId to new ID # (this creates unique IDs for each object)
         }
 
     public String getName() {
@@ -24,6 +32,14 @@ public class Cheese {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
     }
 }
 
